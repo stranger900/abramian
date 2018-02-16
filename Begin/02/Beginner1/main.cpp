5166936a20070d558f38bfa73d8719e13aaed8c0
apetrysh <strangerua900@gmail.com>
2018-02-16 13:53:35 +0200

    demo

---
+++ Begin/02/Beginner1/main.cpp
@@ -0,0 +1,15 @@
+﻿/* Äàíà ñòîðîíà êâàäðàòà a. Íàéòè åãî ïëîùàäü S = a2. */
+
+
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a;
+	cout << "Enter square side size: " << endl;
+	cin >> a;
+	cout << "the area is equal : " << a * a << endl;
+	
+}
