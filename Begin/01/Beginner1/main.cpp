fe9037e132521dee7eaa94bd95f1cb2548807e3f
apetrysh <strangerua900@gmail.com>
2018-02-16 13:47:30 +0200

    demo

---
+++ Begin/01/Beginner1/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a;
+	cout << "Enter square side size: " << endl;
+	cin >> a;
+	cout << "the perimeter is equal : " << a * 4 << endl;
+	
+}
