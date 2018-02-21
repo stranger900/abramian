a9ce99b9e9422a1677b3d393f7d003604fa31704
stranger900 <strangerua900@gmail.com>
2018-02-21 16:49:50 +0200

    update

---
+++ Begin/13/Beginner1/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+void main()
+{
+	double a, b;
+	
+	//Vvod
+
+	cout << "Enter 1 radius: " << endl;
+	cin >> a;
+	cout << "Enter 2 radius: " << endl;
+	cin >> b;
+
+	//Vyvod
+	
+	cout << "s1 =  " << a*a*3.14 << endl;
+	cout << "s2 =  " << b*b*3.14 << endl;
+	cout << "s3 =  " << ((a*a)*3.14) - ((b*b)*3.14) << endl;
+
+}
---
+++ Begin/14/main.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+void main()
+{
+	double a, r;
+	
+	//Vvod
+
+	cout << "Enter length: " << endl;
+	cin >> a;
+	
+	//Vyvod
+	r = a / (2 * 3.14);
+	cout << "R =  " << r << endl;
+	cout << "S =  " << 3.14 * (r*r) << endl;
+	
+}
