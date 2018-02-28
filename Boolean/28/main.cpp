520b95e5e0c71c3cd55f27100fea34292e4ef037
stranger900 <strangerua900@gmail.com>
2018-02-28 16:07:46 +0200

    update

---
+++ Boolean/28/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x, y;
+
+	//Enter data
+	cout << "Enter number X: " << endl;
+	cin >> x;
+	cout << "Enter number Y: " << endl;
+	cin >> y;
+
+	//Output
+
+	cout << ((x<0 && y<0) || (x>0 && y>0)) << endl;
+		
+}
