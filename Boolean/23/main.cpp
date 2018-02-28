14d3b6cde4a83edf8034cbe931d27ef4f9814132
stranger900 <strangerua900@gmail.com>
2018-02-28 15:58:47 +0200

    update

---
+++ Boolean/23/main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x, a, b, c, d;
+
+	//Enter data
+	cout << "Enter number A: " << endl;
+	cin >> x;
+
+	//Calculation
+
+	a = x / 1000;
+	b = (x % 1000) / 100;
+	c = (x % 100) / 10;
+	d = x % 10;
+
+	//Output
+
+	cout << (a==d && b==c) << endl;
+		
+}
---
+++ Boolean/24/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c, d;
+
+	//Enter data
+	cout << "Enter number A: " << endl;
+	cin >> a;
+	cout << "Enter number B: " << endl;
+	cin >> b;
+	cout << "Enter number C: " << endl;
+	cin >> c;
+
+	//Calculation
+
+	d = b*b - 4 * a*c;
+
+	//Output
+
+	cout << (d>0) << endl;
+		
+}
