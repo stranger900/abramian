3f9313e647e4f33451436776fc846e536db4f7a2
stranger900 <strangerua900@gmail.com>
2018-02-28 14:27:43 +0200

    update

---
+++ Boolean/12/main.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c;
+
+	//Enter data
+	cout << "Enter number A: " << endl;
+	cin >> a;
+	cout << "Enter number B: " << endl;
+	cin >> b;
+	cout << "Enter number C: " << endl;
+	cin >> c;
+
+	//Output
+
+	cout << (a>0 && b>0 && c>0) << endl;
+		
+}
