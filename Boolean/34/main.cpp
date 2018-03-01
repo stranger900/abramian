19485959137115d8347db50877a30bfde320fa59
stranger900 <strangerua900@gmail.com>
2018-03-01 14:11:33 +0200

    update

---
+++ Boolean/34/main.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x, y;
+
+	//Enter data
+	cout << "Enter number X: " << endl;
+	cin >> x;
+	cout << "Enter number Y: " << endl;
+	cin >> y;
+
+	//Output
+
+	cout << ((x+y)%2==0) << endl;
+		
+}
