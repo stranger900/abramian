f0ef5ad1669f655b63049912aecd2325f4c55178
stranger900 <strangerua900@gmail.com>
2018-03-01 16:45:05 +0200

    update

---
+++ Boolean/35/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x, y, x1, y1;
+
+	//Enter data
+	cout << "Enter number X: " << endl;
+	cin >> x;
+	cout << "Enter number Y: " << endl;
+	cin >> y;
+	cout << "Enter number X1: " << endl;
+	cin >> x1;
+	cout << "Enter number Y1: " << endl;
+	cin >> y1;
+
+	//Output
+
+	cout << (((x + y) % 2 == 0) == ((x1 + y1) % 2 == 0)) << endl;
+		
+}
