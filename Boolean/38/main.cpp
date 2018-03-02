95debae3594d413de8cd02243948329038c8cf78
stranger900 <strangerua900@gmail.com>
2018-03-02 08:24:22 +0200

    update

---
+++ Boolean/38/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x1, y1, x2, y2;
+
+	//Enter data
+	cout << "Enter number 1: " << endl;
+	cin >> x1;
+	cout << "Enter number Y1: " << endl;
+	cin >> y1;
+	cout << "Enter number X2: " << endl;
+	cin >> x2;
+	cout << "Enter number Y2: " << endl;
+	cin >> y2;
+
+	//Output
+
+	cout << (abs(x2-x1) == abs(y2-y1)) << endl;
+		
+}
