504181448e34a48ec606778578ed359b427dc5d3
stranger900 <strangerua900@gmail.com>
2018-03-06 16:42:14 +0200

    update

---
+++ if/15/main.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c;
+
+	//Enter data
+	cout << "Enter number A: " << endl;
+	cin >> a;
+	cout << "Enter number B: " << endl;
+	cin >> b;
+	cout << "Enter number C: " << endl;
+	cin >> c;
+
+	//Calculation and Output
+
+	if (a >= b && b >= c)
+	{
+		cout << "Sum = " << a+b << endl;
+		
+	}
+	else if (b >= a && a >= c) {
+		cout << "Sum = " << b+a << endl;
+		
+	}
+	else if (a>=c && c>=b) {
+		cout << "Sum = " << a+c << endl;
+		
+	}
+	else if (c >= a && a >= b) {
+		cout << "Sum = " << c+a << endl;
+		
+	}
+	else if (b >= c && c >= a) {
+		cout << "Sum = " << b+c << endl;
+		
+	}
+	else if (c >= b && b >= a) {
+		cout << "Sum = " << c+b << endl;
+		
+	}
+	else {
+		cout << "Something wrong " << endl;
+	}
+	
+}
