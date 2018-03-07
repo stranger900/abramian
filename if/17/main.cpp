4db3d64784effcc17d0e194fd2ac4d742571e49b
stranger900 <strangerua900@gmail.com>
2018-03-07 11:46:22 +0200

    update

---
+++ if/17/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c;
+
+	//Enter data
+	cout << "Enter number A: " << endl;
+	cin >> a;
+	cout << "Enter number B: " << endl;
+	cin >> b;
+	cout << "Enter number C: " << endl;
+	cin >> c;
+
+	//Calculation and Output
+
+	if ((a <= b && b <= c) || (a >= b && b >= c)) {
+		a *= 2;
+		b *= 2;
+		c *= 2;
+		cout << "A = " << a << endl;
+		cout << "B = " << b << endl;
+		cout << "C = " << c << endl;
+	}
+	else {
+		a = -a;
+		b = -b;
+		c = -c;
+		cout << "A = " << a << endl;
+		cout << "B = " << b << endl;
+		cout << "C = " << c << endl;
+	}
+}
---
+++ if/18/main.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c;
+
+	//Enter data
+	cout << "Enter number 1: " << endl;
+	cin >> a;
+	cout << "Enter number 2: " << endl;
+	cin >> b;
+	cout << "Enter number 3: " << endl;
+	cin >> c;
+
+	//Calculation and Output
+
+	if (a==b && a!=c) {
+		cout << "Number 3 is different" << endl;
+	}
+	else if (b == c && b != a) {
+		cout << "Number 1 is different" << endl;
+	}
+	else if (a == c && a != b) {
+		cout << "Number 2 is different" << endl;
+	}
+	else {
+		cout << "Something wrong " << endl;
+	}
+}
