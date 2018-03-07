7158eb5f81df862a14ec221b7c8f41dfe5c3c87b
stranger900 <strangerua900@gmail.com>
2018-03-07 15:45:45 +0200

    update

---
+++ if/19/main.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c, d;
+
+	//Enter data
+	cout << "Enter number 1: " << endl;
+	cin >> a;
+	cout << "Enter number 2: " << endl;
+	cin >> b;
+	cout << "Enter number 3: " << endl;
+	cin >> c;
+	cout << "Enter number 4: " << endl;
+	cin >> d;
+
+	//Calculation and Output
+
+	if (a==b && a!=c && a==d) {
+		cout << "Number 3 is different" << endl;
+	}
+	else if (b == c && c==d && b!=a) {
+		cout << "Number 1 is different" << endl;
+	}
+	else if (a == c && a==d && a != b) {
+		cout << "Number 2 is different" << endl;
+	}
+	else if (a == c && a == b && a != d) {
+		cout << "Number 4 is different" << endl;
+	}
+	else {
+		cout << "Something wrong " << endl;
+		cout << "Probably you entered wrong numbers" << endl;
+	}
+}
---
+++ if/20/main.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a, b, c;
+
+	//Enter data
+	cout << "Enter number A: " << endl;
+	cin >> a;
+	cout << "Enter number B: " << endl;
+	cin >> b;
+	cout << "Enter number C: " << endl;
+	cin >> c;
+
+	//Calculation and Output
+
+	if (abs(b-a) > abs(c-a)) {
+		cout << "Point C is closer. Distance from C to A = " << abs(c - a) << endl;
+	}
+	else if (abs(b - a) < abs(c - a)) {
+		cout << "Point B is closer. Distance from B to A = " << abs(b - a) << endl;
+	}
+	else {
+		cout << "Distances from B to A and from C to A are equal " << endl;
+	}
+}
