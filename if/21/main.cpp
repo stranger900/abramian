94a26b7e5e1cf319187c4d67a1ec2b216caa3c6e
stranger900 <strangerua900@gmail.com>
2018-03-12 08:18:05 +0200

    update

---
+++ if/21/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int x, y;
+
+	//Enter data
+	cout << "Enter number X: " << endl;
+	cin >> x;
+	cout << "Enter number Y: " << endl;
+	cin >> y;
+
+	//Calculation and Output
+
+	if (x == 0 && y == 0) {
+		cout << "Result = " << 0 << endl;
+	}
+	else if (y == 0) {
+		cout << "Result = " << 1 << endl;
+	}
+	else if (x == 0) {
+		cout << "Result = " << 2 << endl;
+	}
+	else {
+		cout << "Result = " << 3 << endl;
+	}
+}
