31e379a96566a5e1b35c509afd58147a3e5eaab5
stranger900 <strangerua900@gmail.com>
2018-03-21 16:46:43 +0200

    update

---
+++ For/01/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int k, n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число K : " << endl;
+	cin >> k;
+	cout << "Внесіть число N : " << endl;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << k << endl;
+	}
+}
