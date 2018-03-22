0dee43460a7fbc1783ac4f5f5c4e80eec2cf31e8
stranger900 <strangerua900@gmail.com>
2018-03-22 15:49:06 +0200

    update

---
+++ For/06/main.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double a;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть вартість цукорок за 1 кг : " << endl;
+	cin >> a;
+
+	for (double i = 1.2; i <= 2; i+=0.2) {
+		cout << "Вартість " << i << " кг цукерок = " << i*a << endl;
+	}
+	
+}
---
+++ For/08/main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int a, b, count = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число А : " << endl;
+	cin >> a;
+	cout << "Внесіть число B : " << endl;
+	cin >> b;
+
+	for (a; a <= b; a++) {
+		cout << "ціле число " << a << endl;
+		count *= a;
+	}
+	cout << "Добуток всіх цілих чисел = " << count << endl;
+}
