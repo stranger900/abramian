a8e03780e939ecc48d4d3f230832f7eda3433b75
stranger900 <strangerua900@gmail.com>
2018-03-26 16:30:54 +0300

    update

---
+++ For/19/main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, factorial = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число N (N > 0): " << endl;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		factorial *= i;
+		//cout << factorial << endl;
+	}
+	cout << endl;
+	cout << "N! = " << factorial << endl;
+	cout << endl;
+}
