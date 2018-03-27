b9553c4f540a2cd19dfcf0891e46e294710d11c3
stranger900 <strangerua900@gmail.com>
2018-03-27 16:17:13 +0300

    update

---
+++ For/21/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, factorial = 1, sum = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число N (N > 0): " << endl;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		factorial *= i;
+		sum += (1/factorial);
+		//cout << "Факторіал = " << factorial << endl;
+		//cout << "Сума = " << sum << endl;
+	}
+	cout << endl;
+	cout << "sum = " << sum << endl;
+	cout << endl;
+}
---
+++ For/22/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double x, n, factorial = 1, sum = 1, step = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число X : " << endl;
+	cin >> x;
+	cout << "Внесіть число N (N > 0): " << endl;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		step *= x;
+		factorial *= i;
+		sum += (step/factorial);
+		//cout << "Факторіал = " << factorial << endl;
+		//cout << "Сума = " << sum << endl;
+	}
+	cout << endl;
+	cout << "sum = " << sum << endl;
+	cout << endl;
+}
