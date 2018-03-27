439a3017ba29da532d5a1596a04c3599f4df7d5a
stranger900 <strangerua900@gmail.com>
2018-03-27 16:06:44 +0300

    update

---
+++ For/20/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, factorial = 1, sum = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число N (N > 0): " << endl;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		factorial *= i;
+		sum += factorial;
+		//cout << "Факторіал = " << factorial << endl;
+		//cout << "Сума = " << sum << endl;
+	}
+	cout << endl;
+	cout << "sum = " << sum << endl;
+	cout << endl;
+}
