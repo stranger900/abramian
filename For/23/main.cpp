317295f37c4ca293ad1a0ceda7b22d06c4d892cd
stranger900 <strangerua900@gmail.com>
2018-03-28 16:57:40 +0300

    update

---
+++ For/23/main.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double x, n, sum = 1 + x, sign = -1, rez;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть число X : " << endl;
+	cin >> x;
+	cout << "Внесіть число N (N > 0): " << endl;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		rez = sign*x*x / (2 * i*(2 * i + 1));
+		
+		cout << "Результат = " << rez << endl;
+	}
+	cout << endl;
+	cout << "sum = " << sum << endl;
+	cout << endl;
+}
