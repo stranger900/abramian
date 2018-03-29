fcb07ddf933958e8fcbe42676bb6eba5ee9a54e0
stranger900 <strangerua900@gmail.com>
2018-03-29 14:36:45 +0300

    update

--- For/23/main.cpp
+++ For/23/main.cpp
@@ -6,22 +6,24 @@ using namespace std;
 
 void main()
 {
-	double x, n, sum = 1 + x, sign = -1, rez;
+	double x, n, sign = -1, rez, sum = 0;
 
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	
+
 	cout << "Внесіть число X : " << endl;
 	cin >> x;
 	cout << "Внесіть число N (N > 0): " << endl;
 	cin >> n;
-
+	sum = x;
 	for (int i = 1; i <= n; i++) {
 		rez = sign*x*x / (2 * i*(2 * i + 1));
-		
-		cout << "Результат = " << rez << endl;
+		sum += rez;
+		sign *= (-1);
+		//cout << "Результат = " << rez << endl;
 	}
 	cout << endl;
 	cout << "sum = " << sum << endl;
 	cout << endl;
+	cout << "sin(x) = " << sin(x) << endl;
 }
