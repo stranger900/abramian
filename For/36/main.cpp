945048b661ed4c7e7cda9734ee7688d595fb0b01
stranger900 <strangerua900@gmail.com>
2018-04-03 16:02:08 +0300

    update

---
+++ For/36/main.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, k, rez, sum=0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N : " << endl;
+	cin >> n;
+	cout << "Внесіть число K : " << endl;
+	cin >> k;
+	cout << endl;
+
+	for (int i = 1; i <= n; i++) {
+		rez = 1;
+		for (int j = 1; j <= k; j++) {
+			rez *= i;
+			cout << "\t rez = " << rez << endl;
+		}
+		sum += rez;
+		cout << "sum = " << sum << endl;
+
+	}
+	
+	cout << endl;
+	cout << "sum = " << sum << endl;
+	cout << endl;
+}
