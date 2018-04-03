ca4cf9c26db6b32dcc990531b4f3641ec7d9ee6e
stranger900 <strangerua900@gmail.com>
2018-04-03 08:59:21 +0300

    update

---
+++ For/32/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double a = 1;
+	int n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N (N > 0) : " << endl;
+	cin >> n;
+	cout << endl;
+
+	for (double i = 1; i <= n; i++) {
+		a = (a + 1) / i;
+		cout << "A" << i << " = " << a << endl;
+	}
+	
+	cout << endl;
+}
