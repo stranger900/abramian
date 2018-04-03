de09155529febdacec22c84e1464b42b23d7cc46
stranger900 <strangerua900@gmail.com>
2018-04-03 08:07:14 +0300

    update

---
+++ For/29/main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double a, b, h, n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N (N > 1) : " << endl;
+	cin >> n;
+	cout << "Внесіть число A : " << endl;
+	cin >> a;
+	cout << "Внесіть число B : " << endl;
+	cin >> b;
+
+	h = (b - a) / n;
+	cout << endl;
+	cout <<"H = "<< h << endl;
+	cout << endl;
+
+	for (double i = 0; i <= b ; i+=h) {
+		double rez = a + i;
+		cout << rez << endl;
+	}
+	
+	cout << endl;
+}
