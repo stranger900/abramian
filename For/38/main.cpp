b6a30a4ef84f65db90b3e306d6cba2a4c0101d7c
stranger900 <strangerua900@gmail.com>
2018-04-04 09:07:25 +0300

    update

---
+++ For/38/main.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, k, rez, sum=0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N : " << endl;
+	cin >> n;
+	
+	cout << endl;
+
+	for (int i = 1; i <= n; i++) {
+		rez = 1;
+		for (int j = ((n+1)-i); j > 0; j--) {
+			rez *= i;
+			cout << "\t rez = " << rez << endl;
+		}
+		sum += rez;
+		cout << "sum = " << sum << endl;
+
+	}
+	
+	cout << endl;
+	cout << "sum = " << sum << endl;
+	cout << endl;
+}
---
+++ For/39/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int a, b, c;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число A : " << endl;
+	cin >> a;
+	cout << "Внесіть число B (B > A): " << endl;
+	cin >> b;
+	cout << endl;
+
+	c = b - a;
+	for (int i = a; i <= b; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << i;
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+}
