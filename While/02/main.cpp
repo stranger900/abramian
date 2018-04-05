68459f753af288797be64c092512dfcecb42ebb9
stranger900 <strangerua900@gmail.com>
2018-04-05 09:56:09 +0300

    update

---
+++ While/02/main.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int a, b, c=0, count=0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число A (A > B): " << endl;
+	cin >> a;
+	cout << "Внесіть число B : " << endl;
+	cin >> b;
+	cout << endl;
+	c = a;
+	while (b <= c && c > 0) {
+		c -= b;
+		count++;
+		
+	}
+	cout << count << endl;
+	cout << endl;
+}
