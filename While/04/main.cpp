1f2a713c6c7568cb453eb3b1285b2f8b8ed217e6
stranger900 <strangerua900@gmail.com>
2018-04-06 14:28:14 +0300

    update

---
+++ While/04/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N : " << endl;
+	cin >> n;
+	
+	cout << endl;
+	
+	while (n>0) {
+		n -= 3;
+		//cout << " n = " << n << endl;
+	}
+	if (n == 0) {
+		cout << "TRUE" << endl;
+	}else{
+	cout << "FALSE" << endl;
+	}
+	cout << endl;
+}
