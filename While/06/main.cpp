acac266e77ddeb85a05ee74a5f2169235e794a2b
stranger900 <strangerua900@gmail.com>
2018-04-06 15:38:36 +0300

    update

---
+++ While/06/main.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, rez=2, dob=1 ;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N : " << endl;
+	cin >> n;
+	
+	cout << endl;
+	rez = n;
+	while (rez>0) {
+		dob *= rez;
+		rez -= 2;
+		//cout << "dob = "<<dob << "\trez = " << rez << endl;
+		if (rez < 2) { break; }
+	}
+	
+	cout <<"N!! = "<< dob << endl;
+	cout << endl;
+}
