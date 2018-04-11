4af2bb20132465ec0785d5fc21ae67758133a788
stranger900 <strangerua900@gmail.com>
2018-04-11 15:31:24 +0300

    update

---
+++ While/15/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double a, sum = 1;
+	int n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число A : " << endl;
+	cin >> a;
+	cout << "Внесіть число N (N > 0) : " << endl;
+	cin >> n;
+	cout << endl;
+
+	while (n > 0) {
+		sum *= a;
+		//cout << "sum = "<<sum << endl;
+		--n;
+	}
+	cout << endl;
+	cout <<"Добуток = "<< sum << endl;
+	cout << endl;
+}
---
+++ While/16/main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double a, sum = 1;
+	int n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число A : " << endl;
+	cin >> a;
+	cout << "Внесіть число N (N > 0) : " << endl;
+	cin >> n;
+	cout << endl;
+
+	while (n > sum) {
+		sum *= a;
+		if (sum > n) {
+			break;
+		}
+		cout << "Квадрат = "<< sum << endl;
+	}
+	
+	cout << endl;
+}
