40e79d5d2a5f0901de7254557484dba4b971d099
stranger900 <strangerua900@gmail.com>
2018-04-12 12:02:44 +0300

    update

---
+++ While/14/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double sum = 0, a, k = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число A : " << endl;
+	cin >> a;
+	cout << endl;
+	
+	while (true) {
+		k += 1;
+		sum += (1/k);
+		cout << " *** Сума = " << sum << "\t K = " << k << endl;
+		if (sum < a && (sum + (1/(k+1)) ) > a)  {
+			cout << "Сума = " << sum << "\t K = " << k << endl;
+			break;
+		}
+				
+	}
+	cout << endl;
+}
