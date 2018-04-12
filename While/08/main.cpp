fe8fc831aec291b98b5b6e0f032a5d696be13a5d
stranger900 <strangerua900@gmail.com>
2018-04-12 08:20:55 +0300

    update

---
+++ While/08/main.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double n, a = 1, dob = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N : " << endl;
+	cin >> n;
+	cout << endl;
+	
+	while (true) {
+		dob = a*a;
+		a++;
+		if (dob == n || (dob < n && (a*a)>n)) {
+			cout << "K = " << dob << endl;
+			break;
+		}
+		//cout << "dob = "<< dob << "\t a = " << a << endl;
+		
+	}
+
+	//cout << "K = " << dob << endl;
+	cout << endl;
+}
