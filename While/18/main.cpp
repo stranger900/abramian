c6122f086a6756b763a3a91d3830b6b7e1a057fa
stranger900 <strangerua900@gmail.com>
2018-04-13 15:06:08 +0300

    update

---
+++ While/18/main.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	__int64 n;
+	int	count = 0, sum = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N: " << endl;
+	cin >> n;
+	cout << endl;
+	
+	while (n>0) {
+		sum += n % 10;
+		
+		n = n / 10;
+		count++;
+		cout << " ***** Сума = " << sum << "\t Кількість цифр = " << count << endl;
+	}
+	cout << "Сума = " << sum << "\t Кількість цифр = " << count << endl;
+	cout << endl;
+}
