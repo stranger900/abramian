41b21ab56bc27395cea1ace972908787e916786e
stranger900 <strangerua900@gmail.com>
2018-04-13 14:41:56 +0300

    update

---
+++ While/17/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int n;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N: " << endl;
+	cin >> n;
+	cout << endl;
+	
+	while (n>0) {
+		cout << n % 10 << endl;
+		n = n / 10;
+	}
+	cout << endl;
+}
