f8072ec0ad84186417aa470f099028be58b00ab4
stranger900 <strangerua900@gmail.com>
2018-04-13 08:30:48 +0300

    update

---
+++ While/16/main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	double s = 10, p;
+	int k = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число P (0 < P < 50): " << endl;
+	cin >> p;
+	cout << endl;
+	
+	while (true) {
+		s += s*(p / 100);
+		k++;
+		cout << " *** Сума = " << s << "\t K = " << k << endl;
+		if (s > 200)  {
+			cout << "Сума = " << s << "\t K = " << k << endl;
+			break;
+		}
+				
+	}
+	cout << endl;
+}
