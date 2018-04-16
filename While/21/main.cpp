55971ca4600b562e068945a19d5845f8d733075e
stranger900 <strangerua900@gmail.com>
2018-04-16 16:04:36 +0300

    update

---
+++ While/21/main.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int n, a;
+	bool zn = false;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N: " << endl;
+	cin >> n;
+	cout << endl;
+
+	while (n>0) {
+		a = n % 10;
+		if (a%2 == 1) {
+			zn = true;
+		}
+		n = n / 10;
+	}
+	if (zn) {
+		cout << "TRUE" << endl;
+	}
+	else {
+		cout << "FALSE" << endl;
+	}
+	cout << endl;
+
+}
