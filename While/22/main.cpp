badabb51d3440a4b63822c1dfe9b32c490716881
stranger900 <strangerua900@gmail.com>
2018-04-17 12:01:39 +0300

    update

---
+++ While/22/main.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int n;
+	double a, i = 2;
+	bool zn = false;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N: " << endl;
+	cin >> n;
+	cout << endl;
+
+	while (n>i) {
+		a = n / i;
+		if ((a-(int)a) == 0) {
+			zn = true;
+			cout << "Дільники = " << a << endl;
+		}
+		
+		i++;
+	}
+	if (zn) {
+		cout << "TRUE" << endl;
+	}
+	else {
+		cout << "FALSE" << endl;
+	}
+	cout << endl;
+
+}
