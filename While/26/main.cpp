658c54d37d0d3868aa6a142291bcf73f9715f64c
stranger900 <strangerua900@gmail.com>
2018-04-17 15:00:25 +0300

    update

---
+++ While/26/main.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	int n, f1 = 0, f2 = 1, rez =0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть число N: " << endl;
+	cin >> n;
+	cout << endl;
+
+	while (f2<n) {
+		f1 = f2;
+		f2 = rez;
+		rez = f1 + f2;
+		//cout << "rez = " << rez << "\t f1 = " << f1 << "\t f2 = " << f2 << endl;
+		if (n < rez) { 
+			cout << "Попереднє число = " << f2 << " \t Наступне число = " << rez << endl; 
+			break; 
+		}
+	}
+	
+	cout << endl;
+
+}
