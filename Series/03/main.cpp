1873f9844912361941bddfc3652e3edd71a9fb74
stranger900 <strangerua900@gmail.com>
2018-04-18 14:22:37 +0300

    update

---
+++ Series/03/main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	const int c = 10;
+	double a[c], sum=0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	for (int i=0; i<10;i++){
+		cout << "Внесіть елемент масиву a[" << i << "] : " << endl;
+		cin >> a[i];
+		sum += a[i];
+	}
+	sum /= c;
+	cout << "Сума = " << sum << endl;
+	cout << endl;
+
+}
