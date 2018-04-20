79df22e3562b1049e3888223895ee47f8561df02
stranger900 <strangerua900@gmail.com>
2018-04-20 10:57:43 +0300

    update

---
+++ Series/06/main.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size;
+	
+	//double arr[size];
+	double dob = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+		
+	cout << "Внесіть N : " << endl;
+	cin >> size;
+
+	double *arr = new double[size];
+
+	srand(time(NULL));
+	for (int i=0; i<size;i++){
+		arr[i] = (0.1 * (rand() % 10)) + (rand() % 10);
+		dob *= arr[i];
+
+		cout << fixed;
+		cout.precision(1);
+		cout << arr[i] << "\t"<< (arr[i] - (int)arr[i]) << endl;
+	}
+
+	delete[] arr;
+	cout << "Добуток = " << dob  << endl;
+	cout << endl;
+
+}
---
+++ Series/07/main.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size;
+	
+	//double arr[size];
+	double sum = 0, fir = 0, sec = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+		
+	cout << "Внесіть N : " << endl;
+	cin >> size;
+
+	double *arr = new double[size];
+
+	srand(time(NULL));
+	for (int i=0; i<size;i++){
+		arr[i] = (0.1 * (rand() % 10)) + (rand() % 10);
+		
+		fir = arr[i] - (arr[i] - (int)arr[i]); //ціла частина
+		sec = (arr[i] - (int)arr[i]);          //дробова частина
+		
+		if ((sec * 10) >= 5) {  //заокруглюєм дробову частину
+			sec = 1;
+		}
+		else {
+			sec = 0;
+		}
+
+		sum += fir + sec; //сума заокруглених значень
+
+		cout << fixed;
+		cout.precision(1);
+		cout << arr[i] << "\t"<< fir + sec << endl;
+	}
+
+	delete[] arr;
+	cout << "Сума = " << sum  << endl;
+	cout << endl;
+
+}
