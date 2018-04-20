c423f2024a958d7fb40002dc63571dba23aae7a1
stranger900 <strangerua900@gmail.com>
2018-04-20 11:24:48 +0300

    update

---
+++ Series/10/main.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size;
+	bool b = false;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+		
+	cout << "Внесіть N : " << endl;
+	cin >> size;
+
+	int *arr = new int[size];
+
+	srand(time(NULL));
+	for (int i=0; i<size;i++){
+		arr[i] = 50 - rand() % 101; //число від -50 до +50
+		//cout << arr[i] << endl;
+
+		if ((arr[i]) >=  0) {  
+			b = true;
+		}
+	}
+
+	delete[] arr;
+	cout << endl;
+	if (b) {
+		cout << "TRUE" << endl;
+	}
+	else {
+		cout << "FALSE" << endl;
+	}
+	cout << endl;
+}
