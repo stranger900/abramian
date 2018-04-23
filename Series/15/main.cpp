4abdc7d2bd8abb15098980f463a8dfb7d73667fc
stranger900 <strangerua900@gmail.com>
2018-04-23 11:43:34 +0300

    update

---
+++ Series/15/main.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size, count = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть K (1 - 100): " << endl;
+	cin >> size;
+	
+	int *arr = new int[size];
+
+	srand(time(NULL));
+	for (int i=1; i <= size; i++){
+		arr[i] = rand() % 101; 
+		cout << i << "\t" << arr[i] << endl;
+		
+		if (arr[i] > size) {  
+			count = arr[i];
+			break;
+		}
+		if (arr[i] == 0) {
+			break;
+		}
+	}
+
+	
+	cout << endl;
+	cout << "Число > К = " << count << endl;
+	cout << endl;
+	delete[] arr;
+}
