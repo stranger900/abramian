329341d2a34313d8ed070397149d5675627e9a30
stranger900 <strangerua900@gmail.com>
2018-04-24 14:41:37 +0300

    update

---
+++ Series/18/main.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size, count = 1, c = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	cout << "Внесіть N (1 - 100): " << endl;
+	cin >> size;
+	
+	int *arr = new int[size];
+
+	for (int i=1; i <= size; i++){
+		arr[i] = count;
+		//cout << i << "\t" << arr[i] <<"\t"<<c<< endl;
+		count++;
+		c++;
+		if (c == 3) {
+			count--;
+			c = 0;
+		}
+		if (arr[i] != arr[i - 1]) {
+			cout <<i<<"\t"<< arr[i] << endl;
+		}
+	}
+
+	cout << endl;
+	delete[] arr;
+}
