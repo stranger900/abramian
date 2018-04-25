b8745dceb8108d488453b3fc0595f0dcb95923f9
stranger900 <strangerua900@gmail.com>
2018-04-25 12:54:31 +0300

    update

---
+++ Series/21/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size, count = 0;
+	bool b = false;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть N (1 - 100): " << endl;
+	cin >> size;
+
+	float *arr = new float[size];
+
+	//srand(time(NULL));
+	for (int i = 0; i < size; i++) {
+		cout << "Внесіть A[" << i << "]: " << endl;
+		cin >> arr[i];
+	}
+
+	for(int j=1; j<=size; j++){
+		if (arr[j] > arr[j - 1]) {
+			//cout << arr[j - 1] << endl;
+			count++;
+		}
+	}
+
+	if (count == (size - 1)) {
+		b = true;
+	}
+
+	if (b) { cout << "TRUE" << endl; }
+	else { cout << "FALSE" << endl; }
+
+	
+	cout << endl;
+	delete[] arr;
+}
