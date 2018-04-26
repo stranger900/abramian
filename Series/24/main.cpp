c001a10f734a8eacff9e71d79cd5c3fd28ac0ac8
stranger900 <strangerua900@gmail.com>
2018-04-26 16:54:47 +0300

    update

---
+++ Series/24/main.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size, sum = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть N (1 - 100): " << endl;
+	cin >> size;
+
+	int *arr = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Внесіть A[" << i << "]: " << endl;
+		cin >> arr[i];
+	}
+
+	for (int j = 0; j < size; j++) {
+		if (arr[j] == 0) {
+			for (int k = j; k < size; k++) {
+				sum += arr[k];
+				j = k + 1;
+				if (arr[k + 1] == 0) { break; break;
+				}
+			}
+		}
+	}
+
+	cout << sum << endl;
+	cout << endl;
+	delete[] arr;
+}
