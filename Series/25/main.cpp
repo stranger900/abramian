167c1c0d73910c346f84d7666f03f32551824710
stranger900 <strangerua900@gmail.com>
2018-04-27 14:46:12 +0300

    update

--- Series/25/main.cpp
+++ Series/25/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void main()
 {
-	int size, sum = 0, previousZero = 0, lastZero = 0;
+	int size, sum = 0, firstZero = 0, lastZero = 0;
 
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
@@ -22,19 +22,15 @@ void main()
 		cin >> arr[i];
 	}
 
-	//for (int i = 0; i < size; i++) {               // find firsZero
-	//	if (arr[i] == 0) { firstZero = i; break; }
-	//}
+	for (int i = 0; i < size; i++) {               // find firsZero
+		if (arr[i] == 0) { firstZero = i; break; }
+	}
 
 	for (int j = size; j > 0; j--) {			  //find lastZero
 		if (arr[j] == 0) { lastZero = j; break; }
 	}
-	
-	for (int m = lastZero - 1; m > 0; m--) {			  //find previousZero
-		if (arr[m] == 0) { previousZero = m; break; }
-	}
-
-	for (int k = previousZero; k < lastZero; k++) {
+			
+	for (int k = firstZero; k <= lastZero; k++) { 
 		sum += arr[k];
 	}
 	
