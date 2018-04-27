666a943f15e0e6d4077d84d9a7ba25b0e4e0af89
stranger900 <strangerua900@gmail.com>
2018-04-27 15:09:03 +0300

    update

---
+++ Series/26/main.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int size, s = 0, k = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть K: " << endl;
+	cin >> k;
+	cout << "Внесіть N: " << endl;
+	cin >> size;
+
+	int *arr = new int[size];
+
+	for (int i = 0; i < size; i++) {			
+		cout << "Внесіть A[" << i << "]: " << endl;
+		cin >> arr[i];
+	}
+
+	
+	for (int m = 0; m < size; m++) {
+		s = arr[m];
+		cout << pow(s, k) << endl;
+	}
+	
+	
+	cout << endl;
+	delete[] arr;
+}
