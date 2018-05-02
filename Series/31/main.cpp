6cd64b2659ae5fbb5789829b00bc2aebbc9d7d96
stranger900 <strangerua900@gmail.com>
2018-05-02 15:55:42 +0300

    update

---
+++ Series/31/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void main()
+{
+	int sizeK, sizeN, s = 0, k = 0;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть K: " << endl;
+	cin >> sizeK;
+	cout << "Внесіть N: " << endl;
+	cin >> sizeN;
+
+	srand(time(NULL));
+
+	int **arr = new int* [sizeK];
+
+	for (int i = 0; i < sizeK; i++) {			
+		arr[i] = new int[sizeN];
+	}
+	////////////////////////////// 
+
+	for (int i = 0; i < sizeK; i++) {
+		for (int j = 0; j < sizeN; j++) {
+			arr[i][j] = rand() % 20;
+			if (arr[i][j] == 2) { k++; }
+			
+			cout << "arr ["<<i<<"]["<<j<<"] = " << arr[i][j] << "\tk = " << k << endl;
+		}
+	}
+	cout << "Кількість = " << k << endl;
+	//////////////////////////////
+	for (int i = 0; i < sizeK; i++) {
+		delete[] arr[i];
+	}
+	cout << endl;
+	delete[] arr;
+}
