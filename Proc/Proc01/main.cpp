15ba6b1481e05284d48dd1517b6020671b30b01b
stranger900 <strangerua900@gmail.com>
2018-05-03 16:22:25 +0300

    update

---
+++ Proc/Proc01/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+double PowerA3(double b) {
+	
+	return b*b*b;
+}
+
+void main()
+{
+	double a;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть A: " << endl;
+	cin >> a;
+
+	cout << PowerA3(a) << endl;
+	
+}
