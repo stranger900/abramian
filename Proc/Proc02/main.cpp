b260a508fc223d0ac1c68ac3343b66312f50a756
stranger900 <strangerua900@gmail.com>
2018-05-04 11:34:43 +0300

    update

---
+++ Proc/Proc02/main.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <ctime> 
+
+using namespace std;
+
+void PowerA234(double a, double b = 1, double c = 1, double d = 1) {
+	b = a*a;
+	c = a*a*a;
+	d = a*a*a*a;
+	cout << a << "^2 = " << b << endl;
+	cout << a << "^3 = " << c << endl;
+	cout << a << "^4 = " << d << endl;
+}
+
+void main()
+{
+	double a, b = 1;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть 1 число: " << endl;
+	cin >> a;
+
+	PowerA234(a);
+
+	cout << "Внесіть 2 число: " << endl;
+	cin >> a;
+
+	PowerA234(a);
+
+	cout << "Внесіть 3 число: " << endl;
+	cin >> a;
+
+	PowerA234(a);
+
+	cout << "Внесіть 4 число: " << endl;
+	cin >> a;
+
+	PowerA234(a);
+
+	cout << "Внесіть 5 число: " << endl;
+	cin >> a;
+
+	PowerA234(a);
+}
