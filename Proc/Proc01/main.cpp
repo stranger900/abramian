ba3aaacfc148d423145f9089b9acbfabb1eb77f3
stranger900 <strangerua900@gmail.com>
2018-05-04 11:26:46 +0300

    update

--- Proc/Proc01/main.cpp
+++ Proc/Proc01/main.cpp
@@ -5,21 +5,40 @@
 
 using namespace std;
 
-double PowerA3(double b) {
-	
-	return b*b*b;
+double PowerA3(double a, double b) {
+	b = a*a*a;
+	return b;
 }
 
 void main()
 {
-	double a;
+	double a, b = 1;
 
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	cout << "Внесіть A: " << endl;
+	cout << "Внесіть 1 число: " << endl;
 	cin >> a;
 
-	cout << PowerA3(a) << endl;
-	
+	cout << PowerA3(a, b) << endl;
+
+	cout << "Внесіть 2 число: " << endl;
+	cin >> a;
+
+	cout << PowerA3(a, b) << endl;
+
+	cout << "Внесіть 3 число: " << endl;
+	cin >> a;
+
+	cout << PowerA3(a, b) << endl;
+
+	cout << "Внесіть 4 число: " << endl;
+	cin >> a;
+
+	cout << PowerA3(a, b) << endl;
+
+	cout << "Внесіть 5 число: " << endl;
+	cin >> a;
+
+	cout << PowerA3(a, b) << endl;
 }
