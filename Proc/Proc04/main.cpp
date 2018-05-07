0116e6a65c496ff17ad92afd35afe7311d7b9346
stranger900 <strangerua900@gmail.com>
2018-05-07 10:03:24 +0300

    update

---
+++ Proc/Proc04/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <cmath> 
+
+using namespace std;
+
+void TrianglePS(double a, double p = 1, double s = 1) {
+	s = (a*a*sqrt(3 ))/ 4;
+	p = 3 * a;
+	cout << "Площа = " << s << endl;
+	cout << "Периметр = " << p << endl;
+	cout << endl;
+}
+
+void main()
+{
+	double a, b, c, d;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Внесіть сторону 1-го трикутника: " << endl;
+	cin >> a;
+
+	cout << "Внесіть сторону 2-го трикутника: " << endl;
+	cin >> b;
+
+	cout << "Внесіть сторону 3-го трикутника: " << endl;
+	cin >> c;
+
+	TrianglePS(a);
+	TrianglePS(b);
+	TrianglePS(c);
+}
