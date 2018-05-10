756c5da6e5b02d5d5b5d00fbde09de61b469b9c4
stranger900 <strangerua900@gmail.com>
2018-05-10 16:07:23 +0300

    update

--- Proc/Proc11/main.cpp
+++ Proc/Proc11/main.cpp
@@ -24,11 +24,11 @@ void Entering(double &a, double &b, double &c, double &d) {
 void main()
 {
 	double a, b, c, d;
-	//double *pa = &a, *pb = &b, *pc = &c, *pd = &d, *px = &x, *py = &y;
+	
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	Entering(a, b, c, d/*pa, pb, pc, pd*/);
+	Entering(a, b, c, d);
 	
 	cout << a<<" " << b << " " << c << " " << d << endl;
 	MinMax(&a, &b);
---
+++ Proc/Proc12/main.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+void SortInc3(double *a, double *b, double *c);
+void Swap(double *x, double *y);
+void Entering(double &a, double &b, double &c) {
+
+	cout << "Внесіть цифру A : " << endl;
+	cin >> a;
+	cout << "Внесіть цифру B : " << endl;
+	cin >> b;
+	cout << "Внесіть цифру C : " << endl;
+	cin >> c;
+	
+	
+}
+
+void main()
+{
+	double a, b, c;
+	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Entering(a, b, c);
+	cout << a << " " << b << " " << c << endl;
+	
+	SortInc3(&a, &b, &c);
+	cout << a << " " << b << " " << c << endl;
+}
+void SortInc3(double *a, double *b, double *c) {
+	double min, max, ave;
+	if (*a > *b) { Swap(a, b); }
+	if (*b > *c) { Swap(b, c); }
+	if (*a > *c) { Swap(a, c); }
+	if (*a > *b) { Swap(a, b); }
+}
+void Swap(double *x, double *y) {
+
+	double temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+
+}
