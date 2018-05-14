8db099a328ebeb02499ba70c1300ebe9fc92c1ba
stranger900 <strangerua900@gmail.com>
2018-05-14 15:13:50 +0300

    update

---
+++ Proc/Proc20/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include <locale>
+#include "windows.h"
+
+using namespace std;
+
+double TriangleP(double &a, double &h);
+
+void Entering(double &a, double &h) {
+
+	cout << "Внесіть основу рівнобедреного трикутника a: " << endl;
+	cin >> a;
+	
+	cout << "Внесіть висоту трикутника h: " << endl;
+	cin >> h;
+	
+}
+
+void main()
+{
+	double a, h;
+	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	for (int i = 0; i < 3; i++) {
+		Entering(a,h);
+		cout << TriangleP(a, h) << endl;
+	}
+}
+double TriangleP(double &a, double &h) {
+	double b = sqrt((a / 2)*(a / 2) + h*h);
+
+	return (b + b + a);
+}
