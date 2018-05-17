34f24cc07943e2b821db873118fa98e44d4446bf
stranger900 <strangerua900@gmail.com>
2018-05-17 16:15:49 +0300

    update

---
+++ Proc/Proc26/main.cpp
@@ -0,0 +1,46 @@
+﻿# include <iostream>
+# include <windows.h>
+# include <cmath>
+# include <ctime>
+#include <vector>
+
+
+using namespace std;
+
+bool IsPower5(int k);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	vector <int> arr(10);
+	int count = 0, k;
+	srand(time(NULL));
+
+	for (int i = 0; i < arr.size(); i++) {
+		arr[i] = rand() % 150;
+	}
+
+	
+	for(auto index : arr) {
+		cout << index << " ";
+		if (index > 0) {
+			if (IsPower5(index)) {
+				count++;
+			}
+		}
+	}
+	cout << endl;
+	cout << "Кількість степеней числа 5 в наборі = " << count << endl;
+	return 0;
+}
+
+bool IsPower5(int k)
+{
+	int i = 1;
+	while (i<k) {
+		i *= 5;
+	}
+	return i == k;
+}
